c043f0def6e4c91b1d642f85590eb2865090465e
inurhuda00 <inurhuda00@gmail.com>
2022-11-16 12:04:34 +0700

    add header

--- src/utils.js
+++ src/utils.js
@@ -15,6 +15,19 @@ function requestFaucet(recipient) {
                     recipient,
                 },
             },
+            headers: {
+                accept: "*/*",
+                "accept-language": "en-US,en;q=0.9",
+                "cache-control": "no-cache",
+                "content-type": "application/json",
+                pragma: "no-cache",
+                "sec-ch-ua": '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": '"macOS"',
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "none",
+            },
         },
         function (error, response, body) {
             const { transferred_gas_objects } = body
